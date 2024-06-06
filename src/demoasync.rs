89fb41187ba8fe85f921afaec1e51bcf988a4f12
wdmbeat <danmei811@icloud.com>
2024-06-06 21:24:40 +0800

    demoasync

--- src/demoasync.rs
+++ src/demoasync.rs
@@ -7,6 +7,8 @@ pub fn Async() -> impl IntoView {
         <h1>"Async"</h1>
         <h2>"demo01"</h2>
         <Demo01/>
+        <h2>"Demo02"</h2>
+        <Demo02/>
     }
 }
 
@@ -53,5 +55,32 @@ pub fn Demo01() -> impl IntoView {
         <br/>
         {is_loading}
         </p>
+    }
+}
+
+async fn important_api_call(name: String) -> String {
+    TimeoutFuture::new(1_000).await;
+    name.to_ascii_uppercase()
+}
+
+#[component]
+pub fn Demo02() -> impl IntoView {
+    let (name, set_name) = create_signal("Bill".to_string());
+    let async_data = create_resource(name, |name| async move { important_api_call(name).await });
+    view! {
+        <input on:input=move |ev| {
+            set_name(event_target_value(&ev));
+        }
+        prop:value=name
+        />
+        <p><code>"name:"</code> {name}</p>
+        <Suspense
+            fallback=move|| view! {<p>"Loading..."</p>}
+        >
+        <p>
+        "Your shouting name is"
+        {move || async_data.get()}
+        </p>
+        </Suspense>
     }
 }
